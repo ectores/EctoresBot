0943948b1438af3cbd9cbd0546ed71ae47f62192
ectores <hromo@fen.uchile.cl>
2023-06-27 00:16:50 -0400

    More config
    Now its more easy config the bot about the commands, jokes and user relationated stuff

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 # EctoresBot
+Hi, this is my first Discord bot project. In Summary, the bot has 3 funtionalities (but 2 are very similars) where: 
 
-This a project for a Discord bot where:
-1. With a command, the bot text "bad jokes".9
-2. When 2 specific users connect a voice channel, the bot enter and reproduce a mp3 file (user1, user2)
-3. When 1 specific user connects a voice channel, the bot enter and reproduce a mp3 file (user3)
+1. With a command, the bot text "bad jokes".
+2. When 2 specific users connect at the same time in a voice channel, the bot enter and reproduce a mp3 file (User1 and User2 -> Sound1)
+3. When 1 specific user connects a voice channel, the bot enter and reproduce a mp3 file (User3 or User4 -> Sound2)
 
 # Before use the bot:
 
 Create a ".env" file with the following variables:
 
 + DISCORD_TOKEN=
-+ DISCORD_GUILD=
-+ DISCORD_JOKES=["Inserte chiste fome 1","Inserte chiste fome 2","easter egg"]
 + DISCORD_USER1=
 + DISCORD_USER2=
 + DISCORD_USER3=
++ DISCORD_USER4=
++ DISCORD_COMMAND=
 
-- DISCORD_TOKEN is the bot token
-- DISCORD_GUILD is the server id, it was thinked for just being used in 1 server
-- DISCORD_JOKES is a list with the different jokes, 
-- DISCORD_USER are the user id for each user.
+- DISCORD_TOKEN is the bot token. See https://discord.com/developers/applications for more information
+- DISCORD_COMMAND is a custom command you can choose :D. It can be "/eb" like EctoresBot :D
+- DISCORD_USER is a UserID for each user. Activate the Developer Mode in Discord for see de ID (is a number with 18 digits)
 
+Also, you will need a "jokes.txt" where are all the jokes you want the bot write like 1 joke per line.
 
-# Requeriments:
+And Finally, move your mp3 files at the mp3 folder and it's done (there is a default.mp3 and it will play when someone wanna play a mp3 but it doesn't exist)
 
-Python
-FFMPEG
-
----README IN PROGRESS---
+# It's done, use the EctoresBot :D
--- bot.py
+++ bot.py
@@ -5,21 +5,23 @@ import discord
 import time
 import io
 
+# from commands import new_command
 from discord.ext import commands
 from dotenv import load_dotenv
 from environ import Env
+from mutagen.mp3 import MP3
 
 env = Env()
 env.read_env()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+COMMAND = os.getenv('DISCORD_COMMAND')
 USER1 = int(os.getenv('DISCORD_USER1'))
 USER2 = int(os.getenv('DISCORD_USER2'))
 USER3 = int(os.getenv('DISCORD_USER3'))
-
-# client = discord.Client(intents=discord.Intents.default())
+USER4 = int(os.getenv('DISCORD_USER4'))
+# print(f"Comando: {COMMAND}")
 
 intents = discord.Intents.default()
 intents.members = True
@@ -29,68 +31,45 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 
- 
 @bot.event
+#When the bot runs, it will print when it is connected and will say in which server it is
 async def on_ready():
-    print(f'{bot.user.name} se ha conectado a Discord!')
+    print(f'{bot.user.name} is connected to Discord!')
  
-    print(f'{bot.user} is connected to the following guild:\n')
+    print(f'{bot.user} is connected to the following Discord Servers:\n')
     for guild in bot.guilds:
         print(f'{guild.name}(id: {guild.id})')
 
 
-
-@bot.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Pium Pium {member.name}, bienvenido al Monke Server Pium Pium!'
-    )
-
-
-
+#Special Use
 @bot.event
 async def on_voice_state_update(member,before,after):
 
     if after.channel == None:
         return
-    
-    # print(f"{member.name} ha cambiado su estado en el canal de voz")
+
+    #When 2 users are connected simultaneously, the bot join at voice channel and will play a sound
     list_id = (USER1,USER2)
     
-    # print(list_id)
-    
-    if member.id in list_id and before.channel == None:
+    channel = bot.get_channel(after.channel.id)
 
-        channel = bot.get_channel(after.channel.id)
+    if member.id in list_id and before.channel == None:
 
         contador = 0
         max_cont = 2
-        # print(f"miembros del canal: {channel.members}")
         
         for _ in channel.members:
-            # print(f"id: {_.id}")
             if _.id in list_id:
                 contador += 1
-                # print("Se ha conectado alguien relevante")
         
         if contador == max_cont:
-            print("Hora de unirme al servidor")
-            vc = await channel.connect()
-            vc.play(discord.FFmpegPCMAudio('testing.mp3'), after=lambda e: print('done', e))
-            time.sleep(7)
-            await vc.disconnect()
+            await bot_music("lol",channel,1)
 
         else:
-            print("Falta gente")
-
-    if member.id == USER3 or member.id == USER1 and before.channel == None:
-        channel = bot.get_channel(after.channel.id)
-        print("Hora de ser un Saco Bot")
-        vc = await channel.connect()
-        vc.play(discord.FFmpegPCMAudio('sus.mp3'), after=lambda e: print('done', e))
-        time.sleep(5)
-        await vc.disconnect()
+            print("I need more users for talking")
+    #When a specific user enter a channel, the bot join at voice channel and will play a sound
+    if (member.id == USER3 or member.id == USER4) and before.channel == None:
+        await bot_music("sus",channel,1)
 
 
 
@@ -99,34 +78,81 @@ async def on_message(message):
 
     if message.author == bot.user:
         return
-    
     message_content = message.content
+    #Here the bot detect a valid command
+    if message_content.startswith(f"{COMMAND}"):
+        list_message = message_content.split(" ")
+        total_message = [i for i in list_message if i != ""]
+        if len(total_message) == 1:
+            await message.channel.send(f"For showing my commands use **{COMMAND} help**")
+            return
+        #See all the mp3 files in the MP3 folder
+        if message_content == f"{COMMAND} playlist":
+            archivos = os.listdir("mp3/")
+            mensaje_1 = "I have the followings audios (separated by space):"
 
-    if message_content == '/sus':
-        user_id = message.author.id
-        guild_id = message.guild.id
+            mensaje_2 = ""
 
-        for guild in bot.guilds:
-            if guild.id == guild_id:
-                for channel in guild.voice_channels:
-                    # print(channel.members)
-                    for member in channel.members:
-                        if member.id == user_id:
-                            print(f"{member.name} me ha invocado")
-                            vc = await channel.connect()
-                            vc.play(discord.FFmpegPCMAudio('sus.mp3'), after=lambda e: print('done', e))
-                            time.sleep(5)
-                            await vc.disconnect()
-                            break
-                break
+            for elem in archivos:
+                mensaje_2 += " " + elem[:len(elem) - 4]
 
-    elif message_content == '/piumpium':
-        jokes = io.open("jokes.txt", mode="r", encoding="utf-8")
-        response = random.choice(jokes.readlines())
-        jokes.close()
-        print("piumpium")
-        await message.channel.send(response)
-
+            await message.channel.send(f"{mensaje_1}{mensaje_2}")
         
- 
+        #See if is a play command
+        elif message_content.startswith(f"{COMMAND} play"):
+            if len(total_message) == len(f"{COMMAND} play"):
+                await message.channel.send("The audio input doesn't exists :(")
+                return
+            #If
+            elif len(total_message) == (len(f"{COMMAND}") + 2):
+                text_channel = message.channel 
+                user_id = message.author.id
+                guild_id = message.guild.id
+
+                for guild in bot.guilds:
+                    if guild.id == guild_id:
+                        for voice_channel in guild.voice_channels:
+                            for member in voice_channel.members:
+                                if member.id == user_id:
+                                    print(f"{member.name}(ID: {member.id}) invoke me")
+                                    await bot_music(total_message[2],voice_channel, text_channel)
+                                    break
+                        break
+        #See if is a joke command
+        elif message_content == f"{COMMAND} joke" or message_content == f"{COMMAND} jokes":
+            jokes = io.open("jokes.txt", mode="r", encoding="utf-8")
+            response = random.choice(jokes.readlines())
+            jokes.close()
+            await message.channel.send(response)
+        #See all the command's bot
+        elif message_content == f"{COMMAND} help":
+            await message.channel.send("Hi, I'm EctoresBOT and I'm here ~~for stealing your money hehe,~~ say dumb jokes, enter at voices channel and play audios. These are my commands:")
+            await message.channel.send(f"**{COMMAND} help**   For showing my commands (recursion :O)")
+            await message.channel.send(f"**{COMMAND} joke**   For telling a ~~dumb~~ joke")
+            await message.channel.send(f"**{COMMAND} play *mp3_file***   For playing a mp3 file in my directory (you should be in a voice channel)")
+            await message.channel.send(f"**{COMMAND} playlist**   For showing all the mp3 files in the folder.")
+
+
+#Funtion for playing a mp3 file in a specific channel
+async def bot_music(music, channel, text):
+    music_format = "mp3/" + music + ".mp3"
+    # print(f"1er formato: {music_format}")
+    file_exist = os.path.isfile(music_format)
+    if not file_exist and text != 1:
+        music_format = "mp3/default.mp3"
+        await text.send("I don't found the mp3, I'm scared D:")
+    channel = channel
+    print(f"audio to play: {music_format}")
+    vc = await channel.connect()
+    vc.play(discord.FFmpegPCMAudio(music_format))
+    time.sleep(mutagen_length(music_format))
+    await vc.disconnect()
+
+#See the mp3 duration
+def mutagen_length(path):
+    audio = MP3(path)
+    length = audio.info.length
+    return length
+
+#Run Barry Run :D
 bot.run(TOKEN)
